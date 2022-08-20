eab1f5b0620e6cd676d2642e0c353afa6bda17aa
1ain1ain1ain <102588382+1ain1ain1ain@users.noreply.github.com>
2022-08-20 11:52:06 +0300

    Update client.go
    Changed comments

--- client/client.go
+++ client/client.go
@@ -48,16 +48,16 @@ func WorkMonitor(arr []string) {
 
 func main() {
 	fmt.Println("Client started")
-	CommandList := []string{"systemctl start nginx.service"}  //This array contains commands that needs to be exicuted on this node
+	CommandList := []string{"systemctl start nginx.service"}  //This array contains commands that need to be executed on this node
 	DeactivateCommandList := []string{"systemctl stop nginx"} //Deactivation commands for commands in CommandList
 	for {
 		CurrentConnection := ConnectionStatus
 		if !CurrentConnection {
-			s, err := net.Dial("tcp", "127.0.0.1:8068") //protocol and IP adr for connection
+			s, err := net.Dial("tcp", "127.0.0.1:8068") // Protocol and IP address for connection
 			if err != nil {
 				fmt.Println(err.Error())
 				WorkMonitor(CommandList)                    // Activate service and host is until connection is restored
-				time.Sleep(time.Duration(10) * time.Second) // seconds before reconnecting
+				time.Sleep(time.Duration(10) * time.Second) // Seconds before reconnecting
 				continue
 			}
 			fmt.Println(s.RemoteAddr().String() + ": connected")
